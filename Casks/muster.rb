b0d9bbd9e802e3c5e632c55d463aa5b04d70f2fb
Muster CI <ci@barleycorn.industries>
2026-04-30 17:05:52 -0700

    Bump muster to 0.2.2

--- Casks/muster.rb
+++ Casks/muster.rb
@@ -1,6 +1,6 @@
 cask "muster" do
-  version "0.2.1"
-  sha256 "455d7c335778744fea1493c5869692c8d9e2a015af3112603bf5192cef36bd8d"
+  version "0.2.2"
+  sha256 "631c3ff8a34f735b8a77c49bd9d62b5eed5e77d576bb6f4760c1678a51a9d857"
 
   url "https://gitlab.com/api/v4/projects/80403280/packages/generic/muster/#{version}/Muster-#{version}.dmg"
   name "Muster"
